0461ed8c71c16dca31eb8ae5b154cbab1a510ac2
Miguel Angel <tinocoarroyomiguel@gmail.com>
2023-03-04 09:22:03 -0500

    fig_2

--- source/report.py
+++ source/report.py
@@ -8,7 +8,6 @@ st.title('Reporte de Informe mediante Gráficos')
 st.subheader('1.  Cantidad de Número de Repitencias por Año de Ingreso:')
 #-------------------------------------------------------------------------------------------
 
-
 df_1 = pd.read_csv('./data/REPITENCIAS_POR_ANIO_INGRESO.csv')
 st.write(df_1) 
 
@@ -17,3 +16,12 @@ fig_1 = px.line(df_1, x = 'Año', y=df_1.columns[1:7], width=900, height=460,mar
 fig_1.update_layout(xaxis_title= 'Año de Ingreso', yaxis_title='Cantidad de Repitencias',
                         legend_title='Número de Repitencias')
 st.plotly_chart(fig_1) 
+
+# -----------------------------------------------------------------------------------------
+df_2 = pd.read_csv('./data/REPITENCIAS_POR_FACULTAD.csv')
+fig_2 = px.line(df_2, x= 'Facultad', y=df_2.columns[1:7], width=1000, height=600, markers=True,
+                    color_discrete_sequence = px.colors.qualitative.Light24_r)
+fig_2.update_layout(xaxis_title= 'Facultad', yaxis_title='Cantidad de Repitencias',
+                        legend_title='Número de Repitencias')
+
+st.plotly_chart(fig_2)
