081193c155a532d36899e0f61b1ac0e2c9e6e04c
Miguel Angel <tinocoarroyomiguel@gmail.com>
2023-03-03 22:18:09 -0500

    qqqqq

--- source/report.py
+++ source/report.py
@@ -8,14 +8,6 @@ st.title('Reporte de Informe mediante Gráficos')
 st.subheader('1.  Cantidad de Número de Repitencias por Año de Ingreso:')
 #-------------------------------------------------------------------------------------------
 
-def load_data(nrows):
-    datos = pd.read_excel('./data/REPITENCIAS_POR_ANIO_INGRESO.xlsx', nrows=nrows)
-    datos.fillna(0, inplace=True)
-    return datos
 
-df_load_state = st.text('Cargando data ...')
-datos = load_data(1000)
-
-if st.checkbox('Mostrar datos crudos'):
-    st.subheader('Datos crudos..')
-    st.write(datos)
+datos = pd.read_excel('./data/REPITENCIAS_POR_ANIO_INGRESO.xlsx')
+st.write(datos) 
