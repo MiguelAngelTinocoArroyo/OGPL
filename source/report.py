492b08573c33c71420313a38b2c636024609ca4b
Miguel Angel <tinocoarroyomiguel@gmail.com>
2023-03-03 22:07:35 -0500

    pipippipiipi

---
+++ source/report.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+from plotly import graph_objects as go
+
+st.title('Reporte de Informe mediante Gráficos')
+
+st.subheader('1. Cantidad de Número de Repitencias por Año de Ingreso:')
+#-------------------------------------------------------------------------------------------
+
+def load_data(nrows):
+    datos = pd.read_excel('./data/REPITENCIAS_POR_ANIO_INGRESO.xlsx')
+    datos.fillna(0, inplace=True)
+    return datos
+
+df_load_state = st.text('Cargando data ...')
+datos = load_data(1000)
+
+if st.checkbox('Mostrar datos crudos'):
+    st.subheader('Datos crudos..')
+    #st.write(datos)
