6633c8bd6c61ad91e80b6111a2f85268617fabaa
Miguel Angel <tinocoarroyomiguel@gmail.com>
2023-03-05 11:51:39 -0500

    de

--- source/report.py
+++ source/report.py
@@ -165,24 +165,11 @@ st.plotly_chart(fig_6)
 
 st.subheader('7. Top 10 de Facultades con mayores Repitencias en total 2023-0:')
 
-@st.cache(allow_output_mutation=True)
-def load_data_7(nrows):
-    datos_7 = pd.read_csv('./data/REPITENCIAS_POR_FACULTAD_Y_EAP.csv', nrows=nrows)
-    datos_7 = datos_7.reset_index(drop=True)
-    return datos_7
-
-df_load_state = st.text('Cargando data ...')
-datos_7 = load_data_7(10)
-
-if st.checkbox('Mostrar datos 7'):
-    st.subheader('Datos')
-    st.write(datos_7)
-
-
 
 df_7 = pd.read_csv('./data/TOTAL_DE_REPITENCIAS_POR_FACULTAD.csv')
 df_7 = df_7.reset_index(drop=True)
 df_7 = df_7.head(10)
+st.write(df_7)
  
 fig_7 = px.line_polar(df_7, r='Total de Repitencias', theta='Facultad',line_close=True,
                     color_discrete_sequence = px.colors.sequential.RdBu_r)
