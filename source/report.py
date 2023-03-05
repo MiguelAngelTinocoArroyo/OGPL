153e8754eb6078b47475c2dd00e513474a1f1db0
Miguel Angel <tinocoarroyomiguel@gmail.com>
2023-03-05 11:54:48 -0500

    fr

--- source/report.py
+++ source/report.py
@@ -167,7 +167,7 @@ st.subheader('7. Top 10 de Facultades con mayores Repitencias en total 2023-0:')
 
 @st.cache(allow_output_mutation=True)
 def load_data_7(nrows):
-    datos_7 = pd.read_csv('./data/REPITENCIAS_POR_FACULTAD_Y_EAP.csv', nrows=nrows)
+    datos_7 = pd.read_csv('./data/TOTAL_DE_REPITENCIAS_POR_FACULTAD.csv', nrows=nrows)
     datos_7 = datos_7.sort_values('Total de Repitencias',ascending=False)
     datos_7 = datos_7.reset_index(drop=True)
     return datos_7
