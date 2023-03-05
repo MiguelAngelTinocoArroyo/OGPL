96494603464886f0848d4e1e6c08beaa69a0b891
Miguel Angel <tinocoarroyomiguel@gmail.com>
2023-03-05 10:29:42 -0500

    qqqq

--- source/report.py
+++ source/report.py
@@ -58,21 +58,22 @@ st.plotly_chart(fig_2)
 # -----------------------------------------------------------------------------------------
 st.subheader('3. Cantidad de Número de Repitencias por Facultad:')
 
-
 @st.cache(allow_output_mutation=True)
 def load_data_3(nrows):
     datos_3 = pd.read_csv('./data/REPITENCIAS_POR_FACULTAD.csv', nrows=nrows)
+    datos_3.fillna(0, inplace=True)
     return datos_3
 
 df_load_state = st.text('Cargando data ...')
-datos_3 = load_data_2(44)
+datos_3 = load_data_3(20)
 
 if st.checkbox('Mostrar datos 3'):
     st.subheader('Datos')
     st.write(datos_3)
 
 df_3 = pd.read_csv('./data/REPITENCIAS_POR_FACULTAD.csv')
-st.write(df_3)
+df_3.fillna(0, inplace=True)
+
 fig_3 = px.line(df_3, x= 'Facultad', y=df_3.columns[1:7], width=1000, height=600, markers=True,
                     color_discrete_sequence = px.colors.qualitative.Light24_r)
 fig_2.update_layout(xaxis_title= 'Facultad', yaxis_title='Cantidad de Repitencias',
