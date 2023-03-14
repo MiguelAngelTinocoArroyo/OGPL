fd4598891b6cd1cad8d0ac66e0333f00c55d374c
Miguel Angel <tinocoarroyomiguel@gmail.com>
2023-03-14 10:34:58 -0500

    es

--- source/report.py
+++ source/report.py
@@ -179,6 +179,22 @@ st.plotly_chart(fig_6)
 
 st.subheader('7. Top 10 de Facultades con mayor número de repitencias:')
 
+###################################################################
+@st.cache(allow_output_mutation=True)
+def load_data_7(nrows):
+    datos_7 = pd.read_csv('./data/TOTAL_DE_REPITENCIAS_POR_FACULTAD.csv', nrows=nrows)
+    datos_7.fillna(0, inplace=True)
+    datos_7 = datos_7.sort_values('Repitencias',ascending=False)
+    return datos_7
+
+df_load_state = st.text('Cargando data ...')
+datos_7 = load_data_7(5)
+
+if st.checkbox('Mostrar datos 7'):
+    st.subheader('Datos 7')
+    st.write(datos_7)
+
+####################################################################
 df_7 = pd.read_csv('./data/TOTAL_DE_REPITENCIAS_POR_FACULTAD.csv')
 df_7 = df_7.sort_values('Repitencias',ascending=False)
 df_7 = df_7.reset_index(drop=True)
