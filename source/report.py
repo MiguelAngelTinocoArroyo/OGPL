9baac1383b779fb29ffff63d6851b03e8eac0758
Miguel Angel <tinocoarroyomiguel@gmail.com>
2023-03-19 21:33:39 -0500

    w

--- source/report.py
+++ source/report.py
@@ -182,7 +182,7 @@ st.subheader('7. Top 10 de Facultades con mayor número de repitencias:')
 ###################################################################
 @st.cache(allow_output_mutation=True)
 def load_data_7(nrows):
-    datos_7 = pd.read_csv('./data/TOTAL_DE_REPITENCIAS_POR_FACULTAD.csv', nrows=nrows)
+    datos_7 = pd.read_csv('./data/TOTAL_REPITENCIAS_POR_FACULTAD.csv', nrows=nrows)
     datos_7 = datos_7.sort_values(by="Repitencias",ascending=False)
     datos_7 = datos_7.reset_index(drop=True)
     return datos_7
@@ -195,7 +195,7 @@ if st.checkbox('Mostrar datos 7'):
     st.write(datos_7)
 
 ####################################################################
-df_7 = pd.read_csv('./data/TOTAL_DE_REPITENCIAS_POR_FACULTAD.csv')
+df_7 = pd.read_csv('./data/TOTAL_REPITENCIAS_POR_FACULTAD.csv')
 df_7 = df_7.sort_values('Repitencias',ascending=False)
 df_7 = df_7.reset_index(drop=True)
 df_7 = df_7.head(10)
